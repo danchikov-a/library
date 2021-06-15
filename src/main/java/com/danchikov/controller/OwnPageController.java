368c27e9dd1b54bd2f103a5ed875d00d69c37e12
danchikov_a <danchikovartem@gmail.com>
2021-06-15 12:28:23 +0300

    Before creating task

--- src/main/java/com/danchikov/controller/OwnPageController.java
+++ src/main/java/com/danchikov/controller/OwnPageController.java
@@ -2,6 +2,7 @@ package com.danchikov.controller;
 
 import com.danchikov.entity.User;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @Controller
 public class OwnPageController {
     @GetMapping("ownPage")
-    public String showList(@ModelAttribute("username") User user){
-        System.out.println(user.getUsername());
+    public String showList(@ModelAttribute("username") User user,Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        model.addAttribute("nick",username);
         return "ownPage";
     }
 }
